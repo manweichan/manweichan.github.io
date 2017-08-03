f4a1f082d369bbf5c4667556deb7cd48fa9bb431
Manwei Chan <manweichan@Manweis-MacBook-Pro.local>
2017-08-02 20:30:39 -0400

    added travel and photo page

---
+++ Pages/photography/Js/script.js
@@ -0,0 +1,21 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+$('.grid').masonry({
+  // set itemSelector so .grid-sizer is not used in layout
+  itemSelector: '.grid-item',
+  // use element for option
+  columnWidth: '.grid-sizer',
+  percentPosition: true
+})
+
+// // jQuery
+// $grid.masonry()
+// // vanilla JS
+// msnry.layout()
