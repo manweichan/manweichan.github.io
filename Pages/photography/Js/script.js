7d66bc0cc3b959de9eafb25fb057d9d0a4a38a74
Manwei Chan <manweichan@Manweis-MBP.home>
2017-08-24 21:46:26 -0400

    Added masonry fix

--- Pages/photography/Js/script.js
+++ Pages/photography/Js/script.js
@@ -7,6 +7,8 @@ function myFunction() {
     }
 }
 
+
+
 // external js: masonry.pkgd.js, imagesloaded.pkgd.js
 
 // init Masonry after all images have loaded
@@ -21,16 +23,32 @@ function myFunction() {
 // external js: masonry.pkgd.js, imagesloaded.pkgd.js
 
 // init Masonry
-var $grid = $('.grid').masonry({
-  itemSelector: '.grid-item',
-  percentPosition: true,
-  columnWidth: '.grid-sizer'
-});
-// layout Masonry after each image loads
-$grid.imagesLoaded().progress( function() {
-  $grid.masonry('layout');
-});  
+// var $grid = $('.grid').masonry({
+//   itemSelector: '.grid-item',
+//   percentPosition: true,
+//   columnWidth: '.grid-sizer'
+// });
+// // layout Masonry after each image loads
+// $grid.imagesLoaded().progress( function() {
+//   $grid.masonry();
+// });  
 
+// var freeMasonry = $('.masonry');
+
+// freeMasonry.imagesLoaded()
+//   .done(function(){
+//     freeMasonry.masonry({
+//       itemSelector: 'img'
+//     });
+//   });
+
+$(document).ready(function () {
+    var $container = $("#container");
+
+    $container.imagesLoaded(function () {
+        $container.masonry();
+    });
+});
 
 // // jQuery
 // $grid.masonry()
