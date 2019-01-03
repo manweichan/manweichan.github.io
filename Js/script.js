1038b8367ec8c037ef4b09566ae386959ec7b155
Manwei <manwei.chan@gmail.com>
2019-01-03 00:55:50 -0500

    Working work and travel sections. Seems to be working for mobile too

--- Js/script.js
+++ Js/script.js
@@ -38,34 +38,6 @@ var scene = new ScrollMagic.Scene({triggerElement: "#triggerh2",  duration: 0, o
         .addIndicators({name: "indicatorh2"}) // add indicators (requires plugin)
         .addTo(controller);
 
-// var wipeAnimation = new TimelineMax()
-//     // animate to second panel
-//         // .to("#slideContainer", 0.5, {z: -150})    // move back in 3D space
-//         // .fromTo(".CLASS", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone})  // in from left
-
-//         .to("#slideContainer", 2,   {y: "-25%"})  // move in to first panel
-//         // .to("#slideContainer", 0.5, {z: 0})       // move back to origin in 3D space
-//         // animate to third panel
-//         // .to("#slideContainer", 0.5, {z: -150, delay: 1})
-//         .to("#slideContainer", 2,   {y: "-50%"})
-//         // .to("#slideContainer", 0.5, {z: 0})
-//         // animate to forth panel
-//         // .to("#slideContainer", 0.5, {z: -150, delay: 1})
-//         .to("#slideContainer", 2,   {y: "-75%"})
-//         // .to("#slideContainer", 0.5, {z: 0});
-
-//       // create scene to pin and link animation
-//       new ScrollMagic.Scene({
-//           triggerElement: "#pinContainer",
-//           triggerHook: "onLeave",
-//           duration: "500%"
-//         })
-//         .setPin("#pinContainer")
-//         .setTween(wipeAnimation)
-//         .addIndicators() // add indicators (requires plugin)
-//         .addTo(controller);
-
-// }
   $(function () { // wait for document ready
     // init
     // var controller = new ScrollMagic.Controller();
@@ -84,6 +56,30 @@ var scene = new ScrollMagic.Scene({triggerElement: "#triggerh2",  duration: 0, o
       })
       .setPin("#pinContainer")
       .setTween(wipeAnimation)
+      .addIndicators() // add indicators (requires plugin)
+      .addTo(controller);
+  });
+
+  $(function () { // wait for document ready
+    // init
+    // var controller = new ScrollMagic.Controller();
+
+    // define movement of panels
+    var wipeAnimationTravel = new TimelineMax()
+      // .fromTo("section.panelTravel.SPDA",    1, {y: "100%"}, {y: "0%", ease: Linear.easeNone, delay: 1})  // in from right
+      .fromTo(".Geneva", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone, delay: 1}) // in from top
+      .fromTo(".Beijing", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone, delay: 1}) // in from top
+      .fromTo(".Annecy", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone, delay: 1}) // in from top
+      .fromTo(".Nepal", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone, delay: 1});  // in from left
+
+    // create scene to pin and link animation
+    new ScrollMagic.Scene({
+        triggerElement: "#pinContainerTravel",
+        triggerHook: "onLeave",
+        duration: "700%"
+      })
+      .setPin("#pinContainerTravel")
+      .setTween(wipeAnimationTravel)
       .addIndicators() // add indicators (requires plugin)
       .addTo(controller);
   });
